b55dce11cb12c654d80cbf6452d5848b91484d2c
Ahsan Ali Butt <ahsanbutt515@icloud.com>
2024-10-12 17:28:28 +0500

    Adding Mail Services

--- src/lib/db/index.tsx
+++ src/lib/db/index.tsx
@@ -1,8 +1,26 @@
-// lib/prisma.ts
+// // lib/prisma.ts
+// import { PrismaClient } from "@prisma/client";
+
+// const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+// export const prisma = globalForPrisma.prisma || new PrismaClient();
+
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+declare global {
+  // Prevents `global.prisma` from being redefined during hot-reloads in development
+  var prisma: PrismaClient | undefined;
+}
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log:
+      process.env.NODE_ENV === "development"
+        ? ["query", "info", "warn", "error"]
+        : ["error"],
+  });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
